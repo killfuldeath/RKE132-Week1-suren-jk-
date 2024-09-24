5a6e8d3b935d49b4393dee5946b92b355fe1eb43
Henri Mänd <henri.mand@tktk.ee>
2024-09-25 00:21:46 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Im commenting, kinda boring
+
+//ma pean nime küsimuse tekitama ja siis see tagastab samat nime tervitusena
+Console.WriteLine("Please enter your name:");
+
+//kui ma kuulsin sihukest asja nagu sõne, ma tõesti arvasin et ma läksin hulluks
+
+string userName;
+
+//ma tean et saan teha seda 'string userName' aga ma olen liiga laisk, vist...
+userName = Console.ReadLine();
+
+//proovisin seda alguses komadega teha, mitte plussidega, liiga harjunud vanade keeltega
+Console.WriteLine("Tere, " + userName + "!");
+
+
+//Ma pole ainuke, kes arvab et interpolatsioon kõlab liigselt fancy, ega?
+Console.WriteLine($"Yo, {userName}!");
